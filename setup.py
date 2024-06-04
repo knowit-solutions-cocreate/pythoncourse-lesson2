36a3b61319cd19af3d4b2d969386fcc9d6e7d62a
Edvard Lindelöf <edvard.lindelof@knowit.se>
2024-06-04 09:23:17 +0200

    dependency version constraints

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 
 setup(
     name='lesson_2_code',
-    version='0.0.1',
+    version='0.1.0',
     python_requires='>=3.12',
     install_requires=[
-        # TODO version constrain everything
-        "sqlalchemy",
+        "sqlalchemy >= 2.0.30, < 2.1",
+        "pandas >= 2.2.2, < 2.3",
     ]
 )
 
