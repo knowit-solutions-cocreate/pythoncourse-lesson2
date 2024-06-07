0c1a037cbff02fa212af2fc35ca25980ab4c8a62
Edvard Lindelöf <edvard.lindelof@knowit.se>
2024-06-07 08:41:02 +0200

    add requests dependency

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
         'setuptools >= 68.2.2, < 69',
         'sqlalchemy >= 2.0.30, < 2.1',
         'pandas >= 2.2.2, < 2.3',
+        'requests >= 2.32.3',
     ],
     entry_points={'console_scripts': ['lesson2=lesson_2_code:cli']}
 )
